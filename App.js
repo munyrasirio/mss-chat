7cc4e6afca97a8db923ea0c30a8ba02616bde900
Munyra Sírio <munyrasirio@gmail.com>
2017-07-07 20:10:27 -0300

    My first commit.

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Chat from './src/page/Chat';
+
+export default class App extends Component {
+	render() {
+		return (
+			<Chat/>
+		)
+	}
+}
---
+++ src/page/Contacts.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,
+		TouchableOpacity, Image, StatusBar}
+from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default class Contacts extends Component {
+  	render() {
+		return (
+			<View style={styles.page}>
+			
+			</View>
+		);
+  	}
+}
+
+const styles = StyleSheet.create({
+	page: {
+		flex: 100
+	},
+
+
+});
